f0972a1e992a0339710e4304c8722ef838e613f6
Joe Maddalone <joe@insert-title.com>
2015-11-24 14:41:16 -0600

    code cleanup

--- lessons/06-named-components/App.js
+++ lessons/06-named-components/App.js
@@ -15,25 +15,11 @@ const Links = () => {
   )
 }
 
-const Home = (props) => {
-  return <h1>HOME</h1>
-}
-
-const HomeBody = (props) => {
-  return <p>This is the home body</p>
-}
-
-const Other = (props) => {
-  return <h1>OTHER</h1>
-}
-
-const OtherBody = (props) => {
-  return <p>This is the other body</p>
-}
-
-const Container = (props) => {
-  return <div>{props.header}{props.body}<Links /></div>
-}
+const Home      = ()      => <h1>HOME</h1>
+const HomeBody  = ()      => <p>This is the home body</p>
+const Other     = ()      => <h1>OTHER</h1>
+const OtherBody = ()      => <p>This is the other body</p>
+const Container = (props) => <div>{props.header}{props.body}<Links /></div>
 
 const App = () => {
   return (
